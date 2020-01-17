28e5c01f4b985a5e56f0d8758a5b9cd1727e5f47
estelamaga <estelinha.mag@hotmail.com>
2020-01-17 14:35:37 -0300

    Mão na massa - Conta Corrente

---
+++ spec/saque_spec.rb
@@ -0,0 +1,11 @@
+
+
+#O que é uma suíte de teste?
+#Uma suíte de testes é uma coleção de casos de teste ou specs destinados a testar um programa para verificar um determinado comportamento.
+
+describe ContaCorrente do
+  describe "Saque" do
+    context "quando o valor é positivo" do
+    end
+  end
+end
