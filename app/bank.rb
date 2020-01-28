e2eeedb837d3eacb66b7fcdad22d324ab022c142
estelamaga <estelinha.mag@hotmail.com>
2020-01-28 13:40:15 -0300

    Herança e a Conta Poupança

--- app/bank.rb
+++ app/bank.rb
@@ -1,17 +1,35 @@
-class ContaCorrente
-  attr_accessor :saldo, :mensagem
+class Conta
+    attr_accessor :saldo, :mensagem
 
   def initialize(saldo)
     self.saldo = saldo
   end
 
-  def saca(valor)
-    if (valor > self.saldo)
-      self.mensagem = "Saldo insuficiente para saque :("
-    elsif (valor > 700)
-      self.mensagem = "Limite máximo por saque é de R$ 700"
-    else
-    self.saldo -= valor
+    def saca(valor, max)
+      if (valor > self.saldo)
+        self.mensagem = "Saldo insuficiente para saque :("
+      elsif (valor > max)
+        self.mensagem = "Limite máximo por saque é de R$  " + max.to_s
+      else
+      self.saldo -= valor
+      end
     end
+end
+
+
+
+class ContaCorrente  <  Conta
+  def saca(valor, max = 700)
+    super
   end
 end
+
+
+class ContaPoupanca  <  Conta
+  def saca(valor, max = 500)
+    super
+  end
+end
+
+# super - palavra reserva do ruby, se passar essa palavra ele vai procurar um mesmo
+# nome do método saca na classe Pai e vi executar esse método.
--- spec/bank/saque_cc_spec.rb
+++ spec/bank/saque_cc_spec.rb
@@ -3,57 +3,57 @@
 #O que é uma suíte de teste?
 #Uma suíte de testes é uma coleção de casos de teste ou specs destinados a testar um programa para verificar um determinado comportamento.
 
-require_relative "../app/bank"
+require_relative "../../app/bank"
 
 describe ContaCorrente do
 
   describe "Saque" do
         context "quando o valor é positivo" do
           before(:all) do
-            @conta = ContaCorrente.new(1000.00)
-            @conta.saca(200.00)
+            @cc = ContaCorrente.new(1000.00)
+            @cc.saca(200.00)
           end
           it "então atualiza saldo" do
-            expect(@conta.saldo).to eql 800.00
+            expect(@cc.saldo).to eql 800.00
           end
         end
 
         context "Quando o saldo é zero" do
           before(:all) do
-            @conta = ContaCorrente.new(0.00)
-            @conta.saca(100.00)
+            @cc = ContaCorrente.new(0.00)
+            @cc.saca(100.00)
           end
           it " então exibe mensagem" do
-            expect(@conta.mensagem).to eql "Saldo insuficiente para saque :("
+            expect(@cc.mensagem).to eql "Saldo insuficiente para saque :("
           end
           it " e o saldo final com zero" do
-            expect(@conta.saldo).to eql 0.00
+            expect(@cc.saldo).to eql 0.00
           end
         end
 
         context"Tenho saldo mas não o suficiente" do
           before(:all) do
-            @conta = ContaCorrente.new(500.00)
-            @conta.saca(501.00)
+            @cc = ContaCorrente.new(500.00)
+            @cc.saca(501.00)
           end
            it " então exibe mensagem" do
-            expect(@conta.mensagem).to eql "Saldo insuficiente para saque :("
+            expect(@cc.mensagem).to eql "Saldo insuficiente para saque :("
           end
           it " e o saldo permanece" do
-            expect(@conta.saldo).to eql 500.00
+            expect(@cc.saldo).to eql 500.00
           end
         end
 
         context"Quando supera o limite por saque" do
           before(:all) do
-            @conta = ContaCorrente.new(1000.00)
-             @conta.saca(701.00)
+            @cc = ContaCorrente.new(1000.00)
+             @cc.saca(701.00)
           end
            it " então exibe mensagem" do
-            expect(@conta.mensagem).to eql "Limite máximo por saque é de R$ 700"
+            expect(@cc.mensagem).to eql "Limite máximo por saque é de R$ 700"
           end
           it "e o saldo permance" do
-            expect(@conta.saldo).to eql 1000.00
+            expect(@cc.saldo).to eql 1000.00
           end
         end
   end
