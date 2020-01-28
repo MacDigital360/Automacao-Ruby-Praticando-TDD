1c3e9754ac97d55ac6487047a266f4f5eed3fc4e
estelamaga <estelinha.mag@hotmail.com>
2020-01-28 12:18:34 -0300

    Organização da estrutura do projeto

--- spec/saque_spec.rb
+++ spec/saque_spec.rb
@@ -3,23 +3,7 @@
 #O que é uma suíte de teste?
 #Uma suíte de testes é uma coleção de casos de teste ou specs destinados a testar um programa para verificar um determinado comportamento.
 
-class ContaCorrente
-  attr_accessor :saldo, :mensagem
-
-  def initialize(saldo)
-    self.saldo = saldo
-  end
-
-  def saca(valor)
-    if (valor > self.saldo)
-      self.mensagem = "Saldo insuficiente para saque :("
-    elsif (valor > 700)
-      self.mensagem = "Limite máximo por saque é de R$ 700"
-    else
-    self.saldo -= valor
-    end
-  end
-end
+require_relative "../app/bank"
 
 describe ContaCorrente do
 
